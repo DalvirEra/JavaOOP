6951883ea5c7c8e1decdef8f00892d78a1a25bd3
Dalvir <ilya.hurs@gmail.com>
2023-05-04 23:54:22 +0300

    Les5

---
+++ Les5/models/TableModel.java
@@ -0,0 +1,68 @@
+package models;
+
+import presenters.Model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+public class TableModel implements Model {
+
+
+    private Collection<Table> tables;
+
+
+    /**
+     * Получение списка всех столиков
+     * @return столики
+     */
+    public Collection<Table> loadTables(){
+        if (tables == null){
+            tables = new ArrayList<>();
+
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+
+        }
+        return tables;
+    }
+
+    /**
+     * Бронирование столика
+     * @param reservationDate Дата бронирования
+     * @param tableNo номер столика
+     * @param name имя клиента
+     * @return номер брони
+     */
+    public int reservationTable(Date reservationDate, int tableNo, String name){
+        for (Table table : tables){
+            if (table.getNo() == tableNo){
+                Reservation reservation = new Reservation(reservationDate, name);
+                table.getReservations().add(reservation);
+                return reservation.getId();
+            }
+        }
+        return -1;
+        //throw new RuntimeException("Невозможно забронировать столик.");
+    }
+
+
+
+    //TODO: Разработать самостоятельно в рамках домашней работы
+    public int deleteReservationTable(int oldReservation){
+        for (Table table : tables){
+            for (Reservation res : table.getReservations()){
+                if (res.getId() == oldReservation){
+                    table.getReservations().remove(res);
+                    System.out.printf("Бронь #%d отменена\n", res.getId());
+                    return 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
